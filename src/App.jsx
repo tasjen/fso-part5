05bb9f1eec3c1ca1c937810d9686142282611bff
tastas000 <TiiLg0o000@gmail.com>
2024-01-03 08:24:32 +0700

    finish 5.3

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,28 @@
 import { useState, useEffect } from 'react';
-import LoginForm from './components/LoginForm';
-import Blogs from './components/Blogs';
 import blogService from './services/blogs';
 import loginService from './services/login';
+import Blog from './components/Blog';
 
 const App = () => {
   const [blogs, setBlogs] = useState([]);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [user, setUser] = useState(null);
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const [url, setUrl] = useState('');
+
 
   useEffect(() => {
     blogService.getAll().then((blogs) => setBlogs(blogs));
 
     const loggedUser = localStorage.getItem('loggedUser');
     if (loggedUser) {
-      const user = JSON.parse(loggedUser)
-      setUser(user)
-      blogService.setToken(user.token)
+      const user = JSON.parse(loggedUser);
+      setUser(user);
+      blogService.setToken(user.token);
     }
   }, []);
-
-  const handleUsername = ({ target }) => {
-    setUsername(target.value);
-  };
-
-  const handlePassword = ({ target }) => {
-    setPassword(target.value);
-  };
 
   const handleLogIn = async (event) => {
     event.preventDefault();
@@ -53,20 +48,86 @@ const App = () => {
   const handleLogOut = () => {
     setUser(null);
     localStorage.clear();
+  };
+
+  const handleNewBlog = () => {
+    blogService.create({title, author, url})
+    setTitle('');
+    setAuthor('');
+    setUrl('');
   }
 
   return (
     <>
       {user === null ? (
-        <LoginForm
-          username={username}
-          password={password}
-          handleUsername={handleUsername}
-          handlePassword={handlePassword}
-          handleLogIn={handleLogIn}
-        />
+        <form onSubmit={handleLogIn}>
+          <h1>log in to application</h1>
+          <div>
+            <label htmlFor={'username'}>username</label>
+            <input
+              id={'username'}
+              value={username}
+              onChange={({ target }) => {
+                setUsername(target.value);
+              }}
+            />
+          </div>
+          <div>
+            <label htmlFor={'password'}>password</label>
+            <input
+              id={'password'}
+              type={'password'}
+              value={password}
+              onChange={({ target }) => {
+                setPassword(target.value);
+              }}
+            />
+          </div>
+          <button type={'submit'}>login</button>
+        </form>
       ) : (
-        <Blogs blogs={blogs} user={user} handleLogOut={handleLogOut} />
+        <>
+          <h2>blogs</h2>
+          <span>{user.name} logged in</span>
+          <button onClick={handleLogOut}>logout</button>
+          <h2>create new</h2>
+          <form onSubmit={handleNewBlog}>
+            <div>
+              <label htmlFor={'title'}>title</label>
+              <input
+                id={'title'}
+                value={title}
+                onChange={({ target }) => {
+                  setTitle(target.value);
+                }}
+              />
+            </div>
+            <div>
+              <label htmlFor={'author'}>author</label>
+              <input
+                id={'author'}
+                value={author}
+                onChange={({ target }) => {
+                  setAuthor(target.value);
+                }}
+              />
+            </div>
+            <div>
+              <label htmlFor={'url'}>url</label>
+              <input
+                id={'url'}
+                value={url}
+                onChange={({ target }) => {
+                  setUrl(target.value);
+                }}
+              />
+            </div>
+            <button type={'submit'}>create</button>
+          </form>
+          {blogs.map((blog) => (
+            <Blog key={blog.id} blog={blog} />
+          ))}
+        </>
       )}
     </>
   );
--- src/services/blogs.js
+++ src/services/blogs.js
@@ -7,9 +7,17 @@ const setToken = (newToken) => {
   token = newToken;
 }
 
-const getAll = () => {
-  const request = axios.get(baseUrl)
-  return request.then(response => response.data)
+const getAll = async () => {
+  const res = await axios.get(baseUrl)
+  return res.data;
 }
 
-export default { setToken, getAll }
+const create = async (blogObject) => {
+  const config = {
+    headers: { Authorization: `Bearer ${token}` }
+  }
+  const res = await axios.post(baseUrl, blogObject, config )
+  return res.data;
+}
+
+export default { setToken, getAll, create }
